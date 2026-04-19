669f218792a3c8ebbe35aa16a17d300d792c6419
Liam <jliam1562@gmail.com>
2026-04-19 08:37:53 +0200

    Update recommendations.js

--- assets/js/recommendations.js
+++ assets/js/recommendations.js
@@ -1,10 +1,5 @@
 const recommendations = [
-  {
-    name: 'Cavalry',
-    desc: 'A nice graphic tool that just recently got bought and made free by Canva, used for creating animations and visual effects. (also pls make sure that when you create a Canva account that you turn off pretty much everything that has to do with data collection)',
-    link: 'https://cavalry.studio/'
-  },
-  {
+    {
     name: 'Godot Engine',
     desc: 'An open-source game engine for creating 2D and 3D games. Its great, I use it myself for "actual" games besides Roblox!!',
     link: 'https://godotengine.org/'
@@ -14,7 +9,12 @@ const recommendations = [
     desc: 'A nice VPN service that can also be free if you dont wanna pay. Id also recommend their other services, I personally use them over providers like Google because they have much better privacy policies :)',
     link: 'https://protonvpn.com/'
   },
-{
+  {
+    name: 'Cavalry',
+    desc: 'A nice graphic tool that just recently got bought and made free by Canva, used for creating animations and visual effects. (also pls make sure that when you create a Canva account that you turn off pretty much everything that has to do with data collection)',
+    link: 'https://cavalry.studio/'
+  },
+  { 
     name: 'Cosmos',
     desc: 'A site similar to Pinterest but in my opinion just nicer.',
     link: 'https://cosmos.so/'
